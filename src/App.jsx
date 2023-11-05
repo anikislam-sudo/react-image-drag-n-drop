664ff30cad8ac9aa95bc9003d873b1ce7be66611
Anik Islam <islammoshiu@gmail.com>
2023-11-05 12:34:12 +0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   const [dragging, setDragging] = useState(false);
   const [draggedImage, setDraggedImage] = useState(null);
   const [draggedIndex, setDraggedIndex] = useState(null);
-  // Handle delete images
+
   const handleDeleteClick = () => {
     const updatedImages = images.filter(
       (image) => !selectCards.some((selected) => selected.id === image.id)
@@ -39,19 +39,17 @@ function App() {
     setImages(updatedImages);
     setSelectCards([]);
   };
-  // Handle drag start
+
   const handleDragStart = (image) => {
     setDragging(true);
     setDraggedImage(image);
   };
 
-  // Handle drag over
   const handleDragOver = (e) => {
     e.preventDefault();
     e?.target?.children[0]?.alt && setDraggedIndex(e?.target?.children[0]?.alt);
   };
 
-  // Handle drop image
   const handleDrop = (targetIndex) => {
     setDragging(false);
 
@@ -65,10 +63,11 @@ function App() {
       setDraggedImage(null);
     }
   };
+
   return (
-    <div className="min-h-screen w-screen flex flex-row items-center justify-center md:p-0 p-4">
-      <section className="lg:w-1/2 md:w-3/4 w-full bg-white rounded-lg shadow">
-        <div className="flex flex-col gap-y-2">
+    <div className="min-h-full w-full flex flex-row items-center justify-center md:p-0 p-4 ">
+      <section className="lg:w-1/2 md:w-3/4 w-full  bg-white rounded-lg shadow">
+        <div className="flex flex-col gap-y-2 ">
           <nav className="py-4 px-6">
             <article className="flex flex-row justify-between items-center">
               <h1 className="text-2xl font-bold">
@@ -99,41 +98,76 @@ function App() {
               </button>
             </article>
           </nav>
+          <hr />
           <section className="h-full w-full p-6">
-            <div
-              className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 gap-6"
-              onDragOver={handleDragOver}
-            >
+            <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 gap-6">
               {images.map((image, index) => (
                 <div
                   key={index}
-                  className={
-                    "group relative before:content-[''] before:absolute before:h-full before:w-full before:rounded-lg before:transition-colors before:cursor-move" +
-                    (index === 0
-                      ? " md:col-span-2 md:row-span-2"
-                      : " col-span-1") +
-                    (selectCards.find((photo) => photo.id === image.id)
-                      ? " opacity-100"
-                      : " hover:before:bg-black/50")
-                  }
+                  className={`group relative before:content-['']  before:absolute before:h-full before:w-full before:rounded-lg before:transition-colors before:cursor-move
+                  ${index === 0 ? "md:col-span-2 md:row-span-2" : "col-span-1"}
+                  ${
+                    selectCards.find((photo) => photo.id === image.id)
+                      ? "opacity-100"
+                      : ""
+                  }`}
                   draggable={true}
                   onDragStart={() => handleDragStart(image)}
                   onDrop={() => handleDrop(index)}
+                  onDragOver={handleDragOver}
                 >
-                  <img
-                    src={image.img}
-                    height={index === 0 ? 390 : 184}
-                    width={index === 0 ? 390 : 184}
-
-                    className={
-                      "h-full w-full max-w-full rounded-lg object-contain border-2" +
-                      " " +
-                      (selectCards.find((photo) => photo.id === image.id) &&
-                        "opacity-70")
+                  <div
+                    className={`h-full w-full max-w-full rounded-lg object-contain border-2 relative
+                    ${
+                      selectCards.find((photo) => photo.id === image.id)
+                        ? "opacity-70"
+                        : ""
+                    }`}
+                  >
+                    <img
+                      src={image.img}
+                      alt={image.id}
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    <div className="border-2  hover:bg-black/50 absolute top-0 left-0 right-0 bottom-0 transition-opacity opacity-0 group-hover:opacity-100"></div>
+                  </div>
+                  <input
+                    type="checkbox"
+                    name={image.id}
+                    id={image.id}
+                    className={`absolute top-4 left-4 h-5 w-5 accent-blue-500 group-hover:opacity-100 transition-opacity delay-100 duration-100 ease-linear cursor-pointer
+                    ${
+                      selectCards.find((photo) => photo.id === image.id)
+                        ? "opacity-100"
+                        : "opacity-0"
+                    }`}
+                    checked={
+                      selectCards.find((photo) => photo.id === image.id)
+                        ? true
+                        : false
                     }
+                    onChange={() => {
+                      if (selectCards.find((photo) => photo.id === image.id))
+                        setSelectCards(
+                          selectCards.filter((photo) => photo.id !== image.id)
+                        );
+                      else setSelectCards([...selectCards, image]);
+                    }}
                   />
+                  {dragging && Number(draggedIndex) === Number(image.id) && (
+                    <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-white border-2  rounded-lg"></div>
+                  )}
                 </div>
               ))}
+              <div className="relative border-2 border-dashed rounded-lg p-4 hover-bg-gray-50 transition-colors ease-linear">
+                <div className="h-full w-full flex flex-col justify-center items-center gap-y-4">
+                  <img
+                    src="/placeholder.png"
+                  
+                  />
+                  <span className="whitespace-nowrap ">Add Images</span>
+                </div>
+              </div>
             </div>
           </section>
         </div>
